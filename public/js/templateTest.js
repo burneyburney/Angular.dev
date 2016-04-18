941ec1c7996fd131b464e88be97cef15c722af76
michael burney <burney318@gmail.com>
2016-04-18 18:14:11 -0500

    completed assignment

--- public/js/templateTest.js
+++ public/js/templateTest.js
@@ -4,10 +4,5 @@
 
 (function() {
     var app = angular.module("templateTest", []);
-    <ul>
-    	<li ng-repeat="num in [1, 2, 3, 4, 5, 6, 7, 8, 9]" ng-if="num % 3 == 0">
-        	{{ num }} is even!
-    	</li>
-	</ul>
 
 })();
